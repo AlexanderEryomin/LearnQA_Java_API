d8076da3c8f4bf5eaca761b663e44b158e5a7405
Eremin-AV <eremin-av@gamma-center.ru>
2023-04-24 20:36:27 +0300

    Exercise 4. Get-request

---
+++ src/test/java/FirstGetRequestTest.java
@@ -0,0 +1,13 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class FirstGetRequestTest {
+    @Test
+    public void firstRequest(){
+        Response response = RestAssured
+                .get(" https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
